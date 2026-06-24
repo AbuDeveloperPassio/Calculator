96d6adeedc8519638e4b739749ddca99a6730c62
Abdurrahman D <rahmannoon12@gmail.com>
2026-06-24 16:38:04 +0530

    first commit

---
+++ dsa.js
@@ -0,0 +1,15 @@
+let arr = [5,7,7,8,8,10];
+let target = 8;
+let storingIndexes = [];
+let start = 0;
+let end = arr.length-1;
+
+
+while(start<=end){
+
+    let mid = Math.floor((start+end)/2);
+
+    if(arr[mid]===target){
+        
+    }
+}
